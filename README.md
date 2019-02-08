b6b8d9b40df2c0f997eb7ee23b965a631b57b921
AustinLeeGordon <gordon.austin2@gmail.com>
2019-02-08 14:17:10 -0600

    Added option for Do Not Track

--- README.md
+++ README.md
@@ -17,12 +17,22 @@ module.exports = {
     {
       resolve: "gatsby-plugin-hubspot",
       options: {
-          trackingCode: "1234567"
+          trackingCode: "1234567",
+          // Setting this parameter is optional
+          respectDNT: true
       },
     },
   ]
 }
 ```
+
+### Options
+
+#### respectDNT
+
+By enabling this option, visitors with "Do Not Track" enabled will have a `__hs_do_not_track` cookie placed in their browser. This prevents the HubSpot tracking code from sending any information for the visitor.
+
+More information about HubSpot cookies and privacy can be found in the [HubSpot Tracking Code API documentation](https://developers.hubspot.com/docs/methods/tracking_code_api/tracking_code_overview).
 
 ## License
 
--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 export function onRenderBody({ setPostBodyComponents }, pluginOptions) {
     if (process.env.NODE_ENV === 'production') {
-        let { trackingCode } = pluginOptions;
+        let { trackingCode, respectDNT } = pluginOptions;
 
         if (trackingCode === undefined) {
             console.error('No HubSpot tracking code provided.');
+        }
+
+        if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
+            if (window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.external.msTrackingProtectionEnabled()) {
+                _hsq.push(['doNotTrack']);
+            }
         }
 
         setPostBodyComponents([
@@ -19,6 +25,12 @@ export function onRenderBody({ setPostBodyComponents }, pluginOptions) {
                     __html: `
                         var _hsq = window._hsq = window._hsq || [];
                         _hsq.push(['setPath', window.location.pathname + window.location.search + window.location.hash]);
+                        ${respectDNT === true ? `if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
+                            if (window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.external.msTrackingProtectionEnabled()) {
+                                _hsq.push(['doNotTrack']);
+                            }
+                        }` : ``}
+                        
                     `
                 }}
             />,
