13a1ada18f3f9dfb97af16085ffe904de5ca2fa2
AustinLeeGordon <gordon.austin2@gmail.com>
2019-02-14 08:00:05 -0600

    Remove duplicate DNT check

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -8,12 +8,6 @@ export function onRenderBody({ setPostBodyComponents }, pluginOptions) {
             console.error('No HubSpot tracking code provided.');
         }
 
-        if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
-            if (window.doNotTrack == "1" || navigator.doNotTrack == "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.external.msTrackingProtectionEnabled()) {
-                _hsq.push(['doNotTrack']);
-            }
-        }
-
         setPostBodyComponents([
             <script
                 type="text/javascript"
