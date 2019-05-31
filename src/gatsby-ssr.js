d2f3a5e4f3ed6f100d5cc6811af354f410af1c6a
AustinLeeGordon <gordon.austin2@gmail.com>
2019-05-31 09:48:06 -0500

    Move to gatsby package babel setup, add key to script, trim whitespace from script innerHTML

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { oneLine } from 'common-tags';
 
 export function onRenderBody({ setPostBodyComponents }, pluginOptions) {
     if (process.env.NODE_ENV === 'production') {
         let { trackingCode, respectDNT } = pluginOptions;
-
         if (trackingCode === undefined) {
             console.error('No HubSpot tracking code provided.');
         }
-
         setPostBodyComponents([
             <script
                 type="text/javascript"
                 id="hs-script-loader"
+                key={`gatsby-plugin-hubspot`}
                 async
                 defer
                 src={`//js.hs-scripts.com/${trackingCode}.js`}
                 dangerouslySetInnerHTML={{
-                    __html: `
+                    __html: oneLine`
                         var _hsq = window._hsq = window._hsq || [];
                         _hsq.push(['setPath', window.location.pathname + window.location.search + window.location.hash]);
                         ${respectDNT === true ? `if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
